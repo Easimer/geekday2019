8c5d114baade765987f047f8797161b9f3a71a34
Rőczey Barnabás <roczey.barnabas@gmail.com>
2019-11-23 10:50:04 +0100

    Basic junk evolution

---
+++ gameclient/evolution.cpp
@@ -0,0 +1,54 @@
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <utility>
+#include <random>
+#include "evolution.h"
+
+std::vector<Trait> readFile(const char* fileName) {
+	ifstream infile;
+	infile.open(fileName);
+
+	std::string line;
+	std::vector<Trait> traits();
+	while (std::getline(infile, line))
+	{
+		std::istringstream iss(line);
+		int value;
+		float stepsize
+		if (!(iss >> value >> stepsize)) { break; }
+		traits.push_back({ value, stepsize });
+	}
+
+	infile.close();
+}
+
+void saveFile(std::vector<Trait> traits, const char* fileName) {
+	ofstream outfile;
+	outfile.open(fileName);
+	for (Trait& trait : traits)
+	{
+		outfile << trait.value << " " << trait.step << std::endl;
+	}
+	outfile.close();
+}
+
+std::vector<Trait> mutatedCopy(std::vector<Trait> traits) {
+	std::random_device rd;
+	std::mt19937 e2(rd());
+	std::uniform_real_distribution<> dist(0.5, 2);
+	std::uniform_real_distribution<> dist2(0, 1);
+
+	std::vector<Trait> newTraits(traits.size());
+	for (Trait& trait : traits)
+	{
+		float stepsize = trait.step * dist(e2);
+		int value = trait.value;
+		if (dist2(e2) > 0.5) value += stepsize;
+		else value -= stepsize;
+		newTraits.push_back({ value, stepsize });
+	}
+
+	return newTraits;
+}
