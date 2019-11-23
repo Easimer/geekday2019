f8f29fa637cde44a654341f4603a3ddeb33c9002
Rőczey Barnabás <roczey.barnabas@gmail.com>
2019-11-23 13:34:04 +0100

    Curve thing

---
+++ gameclient/catmullromcurve.cpp
@@ -0,0 +1,68 @@
+#include "catmullromcurve.h"
+
+static const float alpha = 0.5f;
+
+float GetT(float t, Point p0, Point p1)
+{
+	float deltaX = p1.X - p0.X;
+	float deltaY = p1.Y - p0.Y;
+	float a = deltaX * deltaX + deltaY * deltaY;
+	float b = sqrt(a);
+	//float c = Mathf.Pow(b, alpha);
+	float c = sqrt(b);
+
+
+	return (c + t);
+}
+
+void CatmulRom(int amountOfPoints, Point* pointArray, Point p0, Point p1, Point p2, Point p3)
+{
+	float t0 = 0.0f;
+	float t1 = GetT(t0, p0, p1);
+	float t2 = GetT(t1, p1, p2);
+	float t3 = GetT(t2, p2, p3);
+
+	int i = 0;
+	float step = (t2 - t1) / amountOfPoints;
+	for (float t = t1; t < t2; t += float)
+	{
+		float tmp1 = (t1 - t) / (t1 - t0);
+		float tmp2 = (t - t0) / (t1 - t0);
+		float X = tmp1 * p0.X + tmp2 * p1.X;
+		float Y = tmp1 * p0.Y + tmp2 * p1.Y;
+		Point A1 = { X, Y };
+
+
+		float tmp1 = (t2 - t) / (t2 - t1);
+		float tmp2 = (t - t1) / (t2 - t1);
+		float X = tmp1 * p1.X + tmp2 * p2.X;
+		float Y = tmp1 * p1.Y + tmp2 * p2.Y;
+		Point A2 = { X, Y };
+
+		float tmp1 = (t3 - t) / (t3 - t2);
+		float tmp2 = (t - t2) / (t3 - t2);
+		float X = tmp1 * p2.X + tmp2 * p3.X;
+		float Y = tmp1 * p2.Y + tmp2 * p3.Y;
+		Point A3 = { X, Y };
+
+		float tmp1 = (t2 - t) / (t2 - t0);
+		float tmp2 = (t - t0) / (t2 - t0);
+		float X = tmp1 * A1.X + tmp2 * A2.X;
+		float Y = tmp1 * A1.Y + tmp2 * A2.Y;
+		Point B1 = { X, Y };
+
+		float tmp1 = (t3 - t) / (t3 - t1);
+		float tmp2 = (t - t1) / (t3 - t1);
+		float X = tmp1 * A2.X + tmp2 * A3.X;
+		float Y = tmp1 * A2.Y + tmp2 * A3.Y;
+		Point B2 = { X, Y };
+
+		float tmp1 = (t2 - t) / (t2 - t1);
+		float tmp2 = (t - t1) / (t2 - t1);
+		float X = tmp1 * B1.X + tmp2 * B2.X;
+		float Y = tmp1 * B1.Y + tmp2 * B2.Y;
+		Point C = { X, Y };
+
+		pointArray[i++] = C;
+	}
+}
---
+++ gameclient/catmullromcurve.h
@@ -0,0 +1,7 @@
+
+static Point {
+	float X;
+	float Y;
+}
+
+void CatmulRom(int amountOfPoints, Point* pointArray, Point p0, Point p1, Point p2, Point p3);
